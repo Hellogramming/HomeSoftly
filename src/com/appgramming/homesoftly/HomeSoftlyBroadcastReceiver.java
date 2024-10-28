c67ff80e5bac93d8dfa39a003060d6b705f65c01
TechAurelian <dev@techaurelian.com>
2024-10-28 15:48:01 +0200

    First commit

---
+++ src/com/appgramming/homesoftly/HomeSoftlyBroadcastReceiver.java
@@ -0,0 +1,16 @@
+package com.appgramming.homesoftly;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+
+public class HomeSoftlyBroadcastReceiver extends BroadcastReceiver {
+
+	@Override
+	public void onReceive(Context context, Intent arg1) {
+		Intent i = new Intent(context, MainActivity.class);
+		i.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+        context.startActivity(i);  
+	}
+
+}
